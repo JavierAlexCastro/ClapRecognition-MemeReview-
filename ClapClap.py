d32ef8fbe1460766257f27bfcda8e88dbf1f5f83
JavierAlexCastro <8484+JavierAlexCastro@users.noreply.github.com>
2018-04-22 19:04:09 -0500

    Adding files

---
+++ ClapClap.py
@@ -0,0 +1,140 @@
+import pyaudio
+import struct
+import math
+import time
+import winsound
+
+#adjust as needed
+INITIAL_TAP_THRESHOLD = 0.10
+
+#time of each clap
+CLAP = 0
+#determines index of SOUND_LIST
+SOUND_COUNT = 0
+#holds different "meme review" sounds
+SOUND_LIST = ['meme1.wav', 'meme2.wav' , 'meme3.wav', 'meme4.wav' , 'meme5.wav']
+
+FORMAT = pyaudio.paInt16 
+SHORT_NORMALIZE = (1.0/32768.0)
+CHANNELS = 2
+RATE = 44100  
+INPUT_BLOCK_TIME = 0.05
+INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
+# if we get this many noisy blocks in a row, increase the threshold
+OVERSENSITIVE = 15.0/INPUT_BLOCK_TIME                    
+# if we get this many quiet blocks in a row, decrease the threshold
+UNDERSENSITIVE = 120.0/INPUT_BLOCK_TIME 
+# if the noise was longer than this many blocks, it's not a 'tap'
+MAX_TAP_BLOCKS = 0.15/INPUT_BLOCK_TIME
+
+
+def get_rms( block ):
+    # RMS amplitude is defined as the square root of the 
+    # mean over time of the square of the amplitude.
+    # so we need to convert this string of bytes into 
+    # a string of 16-bit samples...
+
+    # we will get one short out for each 
+    # two chars in the string.
+    count = len(block)/2
+    format = "%dh"%(count)
+    shorts = struct.unpack( format, block )
+
+    # iterate over the block.
+    sum_squares = 0.0
+    for sample in shorts:
+        # sample is a signed short in +/- 32768. 
+        # normalize it to 1.0
+        n = sample * SHORT_NORMALIZE
+        sum_squares += n*n
+
+    return math.sqrt( sum_squares / count )
+
+def increase_count(time):
+    global CLAP
+    global SOUND_COUNT
+    global SOUND_LIST
+    
+    if(CLAP==0): #if first clap
+       CLAP=time
+    elif((time-CLAP)<=2): #if second clap within 2 seconds of first one
+        print("meme review!")
+        #put your path in here
+        winsound.PlaySound('C:/Users/User/Documents/ClapRecognition-MemeReview-/'+SOUND_LIST[SOUND_COUNT%5], winsound.SND_FILENAME)
+        SOUND_COUNT+=1
+        CLAP=0 #reset clap counter
+    else: #second clap more than 2 seconds apart from first clap
+        print("too slow..")
+        CLAP=0
+
+class TapTester(object):
+    def __init__(self):
+        self.pa = pyaudio.PyAudio()
+        self.stream = self.open_mic_stream()
+        self.tap_threshold = INITIAL_TAP_THRESHOLD
+        self.noisycount = MAX_TAP_BLOCKS+1 
+        self.quietcount = 0 
+        self.errorcount = 0
+        
+    def stop(self):
+        self.stream.close()
+
+    def find_input_device(self):
+        device_index = None            
+        for i in range( self.pa.get_device_count() ):     
+            devinfo = self.pa.get_device_info_by_index(i)   
+            print( "Device %d: %s"%(i,devinfo["name"]) )
+
+            for keyword in ["mic","input"]:
+                if keyword in devinfo["name"].lower():
+                    print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
+                    device_index = i
+                    return device_index
+
+        if device_index == None:
+            print( "No preferred input found; using default input device." )
+
+        return device_index
+
+    def open_mic_stream( self ):
+        device_index = self.find_input_device()
+
+        stream = self.pa.open(   format = FORMAT,
+                                 channels = CHANNELS,
+                                 rate = RATE,
+                                 input = True,
+                                 input_device_index = device_index,
+                                 frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
+
+        return stream
+
+    def tapDetected(self): #DETECTED
+        increase_count(int(round(time.time())))
+
+    def listen(self):
+        try:
+            block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
+        except e:
+            # dammit. 
+            self.errorcount += 1
+            print( "(%d) Error recording: %s"%(self.errorcount,e) )
+            self.noisycount = 1
+            return
+
+        amplitude = get_rms( block )
+        if amplitude > self.tap_threshold:
+            # noisy block
+            self.quietcount = 0
+            self.noisycount += 1
+        else:            
+            # quiet block.
+            if 1 <= self.noisycount <= MAX_TAP_BLOCKS:
+                self.tapDetected()
+            self.noisycount = 0
+            self.quietcount += 1
+
+if __name__ == "__main__":
+    tt = TapTester()
+    
+    while(1):
+        tt.listen()
